13cf076eee16d94120b9385e8dc4d4798284796f
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-08-30 12:07:49 +0300

    modified spacing

--- private/functions.php
+++ private/functions.php
@@ -3,10 +3,11 @@
 function url_for($script_path) {
   // add the leading '/' if not present
   if($script_path[0] != '/') {
-    $script_path = "/" . $script_path;
+    $script_path = "/" . $script_path; 
   }
   return WWW_ROOT . $script_path;
 }
+
 
 function u($string="") {
   return urlencode($string);
